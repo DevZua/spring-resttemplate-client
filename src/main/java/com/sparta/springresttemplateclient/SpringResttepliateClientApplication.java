a245208739ee857e86e5a0082e1ee24bad299df2
Zua <gimzua0228@gmail.com>
2024-07-11 23:21:42 +0900

    비즈니스 로직 구현 완료

--- src/main/java/com/sparta/springresttemplateclient/SpringResttepliateClientApplication.java
+++ src/main/java/com/sparta/springresttemplateclient/SpringResttepliateClientApplication.java
@@ -1,4 +1,4 @@
-package com.sparta.springresttepliateclient;
+package com.sparta.springresttemplateclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/sparta/springresttemplateclient/SpringResttepliateClientApplicationTests.java
+++ src/test/java/com/sparta/springresttemplateclient/SpringResttepliateClientApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sparta.springresttepliateclient;
+package com.sparta.springresttemplateclient;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
